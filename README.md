9a796d3ecd71a6c758d3f287c53d08b0d58f369e
Mitchell Busby <mitchbusby@gmail.com>
2019-04-28 11:31:27 +1000

    Initial work, and some readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# commuter
+
+Curious to work out my commuting patterns. Gonna use my Opal data to find out spikes in commute times. Mainly focussed on the morning commute.
--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 import dotenv from 'dotenv';
 import Opaler from 'opaler';
+import {DateTime} from 'luxon';
 
 dotenv.config({
     path: 'dev.env'
 });
 
-const opaler = new Opaler("", "")
+const isATrip = (transaction) => !!transaction.journey;
+const isAMorningCommute = transaction => {
+    const timestamp = DateTime.fromSeconds(transaction.timestamp);
+    const isWithinTimeBound = timestamp.hour >= 6 && timestamp.hour <= 10;
+    // TODO: weekly bounds
+    return isWithinTimeBound;
+}
+
+const getPagedTransactions = async (opaler, page, cardIdx) => {
+    const transactions = await opaler.getTransactions({
+        pageIndex: page,
+        cardIndex: cardIdx,
+    });
+
+    const transactionsFiltered = transactions
+        .filter(isATrip)
+        .filter(isAMorningCommute);
+
+    return transactionsFiltered;
+}
+
+const getTransactions = async () => {
+    const opaler = new Opaler(process.env.OPAL_USERNAME, process.env.OPAL_PASSWORD);
+
+    const cards = await opaler.getCards();
+
+    // Workaround to grab my non-student card
+    const selectedCard = cards.find(card => card.cardState !== 'HOTLISTED');
+
+    const transactionsFiltered = await getPagedTransactions(opaler, 1, selectedCard.cardIndex);
+
+    transactionsFiltered.forEach(tx => {
+        console.log(tx.summary);
+    });
+}
+
+getTransactions();
